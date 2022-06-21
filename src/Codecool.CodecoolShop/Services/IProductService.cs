5bb6a62963bb0dbbd80121689235c1568a2876b9
adrianzych2clo <adrianzych2@gmail.com>
2022-06-21 10:12:13 +0200

    Changed names od products

---
+++ src/Codecool.CodecoolShop/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Codecool.CodecoolShop.Models;
+
+namespace Codecool.CodecoolShop.Services
+{
+    public interface IProductService
+    {
+        public ProductCategory GetProductCategory(int categoryId);
+
+        public IEnumerable<Product> GetProductsForCategory(int categoryId);
+    }
+}
