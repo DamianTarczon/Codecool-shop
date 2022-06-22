80f5567f29ba0cb3c59cedf4d411b696222b4f8b
adrianzych2clo <adrianzych2@gmail.com>
2022-06-22 13:47:38 +0200

    Added endpoint to ordercontroller (CreditCardDetails) and made View

--- src/Codecool.CodecoolShop/Models/CreditCard.cs
+++ src/Codecool.CodecoolShop/Models/CreditCard.cs
@@ -11,28 +11,28 @@ namespace Codecool.CodecoolShop.Models
         [Required(ErrorMessage = "Please enter valid card number")]
         [StringLength(16, MinimumLength = 16, ErrorMessage = "Card number should consist of 16 digits")]
         [RegularExpression("^[0-9]*$", ErrorMessage = "Card number must be numeric")]
-        private SecureString CardNumber { get; set; }
+        public String? CardNumber { get; set; }
 
         [Required(ErrorMessage = "Please enter your first name")]
-        [StringLength(2, MinimumLength = 49, ErrorMessage = "Please enter valid first name" )]
-        private string FirstName { get; set; }
+        [StringLength(49, MinimumLength = 2, ErrorMessage = "Please enter valid first name" )]
+        public string? FirstName { get; set; }
 
         [Required(ErrorMessage = "Please enter your last name")]
-        [StringLength(2, MinimumLength = 49, ErrorMessage = "Please enter valid last name")]
-        private string LastName { get; set; }
+        [StringLength(49, MinimumLength = 2, ErrorMessage = "Please enter valid last name")]
+        public string? LastName { get; set; }
 
         
         [Required(ErrorMessage = "Please enter your expire month")]
         [Range(1, 12, ErrorMessage = "Please enter valid month number")]
-        private int ExpireMonth { get; set; }
+        public int? ExpireMonth { get; set; }
 
         [Required(ErrorMessage = "Please enter your expire year")]
         [Range(2022, 2027, ErrorMessage = "Please enter valid month year")]
-        private int ExpireYear { get; set; }
+        public int? ExpireYear { get; set; }
 
         [Required(ErrorMessage = "Please enter your CVV")]
         [StringLength(3, MinimumLength = 3, ErrorMessage = "CVV should consist of 3 digits")]
         [RegularExpression("^[0-9]*$", ErrorMessage = "CVV must be numeric")]
-        private SecureString CVV { get; set; }
+        public String? CVV { get; set; }
     }
 }
