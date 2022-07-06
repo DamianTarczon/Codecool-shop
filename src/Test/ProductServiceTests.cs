821900e698114e911db02bd4db0898b30808870e
Damian Tarczoń <tarczon96@gmail.com>
2022-07-06 15:03:33 +0200

    test project added

---
+++ src/Test/ProductServiceTests.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class ProductServiceTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
